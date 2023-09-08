4c8ed44079fb3f6b4b24d0fdfb5986289cac4ba8
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2023-09-08 10:29:22 +0300

    chore: Set up routers

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const Message = require('../models/message');
 
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+router.get('/', async (req, res, next) => {
+  const messages = await Message.find().sort('-createdAt');
+  res.render('index', { messages });
+});
+
+router.get('/new', (req, res, next) => { 
+  res.render('form');
+});
+
+router.post('/new', async (req, res, next) => { 
+  const { sender, text } = req.body;
+  const message = new Message({ sender, text });
+  await message.save();
+  res.redirect('/');
 });
 
 module.exports = router;
