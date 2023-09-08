8fe27f5a5c06f6062e3d96bf047f6ff7fbef7b17
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2023-09-08 14:55:45 +0300

    chore: Validate form data

--- routes/index.js
+++ routes/index.js
@@ -5,18 +5,53 @@ const router = express.Router();
 
 router.get('/', async (req, res, next) => {
   const messages = await Message.find().sort('-createdAt');
-  res.render('index', {title: 'Message Board', messages });
+  res.render('index', { title: 'Message Board', messages });
 });
 
-router.get('/new', (req, res, next) => { 
-  res.render('form', { title: 'New Message' });
+router.get('/new', (req, res, next) => {
+  res.render('form', { title: 'New Message', sender: '', text: '' });
 });
 
-router.post('/new', async (req, res, next) => { 
+function validateFormData(req, res, next) {
   const { sender, text } = req.body;
-  const message = new Message({ sender, text });
-  await message.save();
-  res.redirect('/');
+  let senderErr = '';
+  let textErr = '';
+
+  if (!sender) {
+    senderErr = 'Name is required';
+  } else if (sender.length < 2 || sender.length > 100) {
+    senderErr = 'Name must be between 2 and 100 characters';
+  }
+
+  if (!text) {
+    textErr = 'Message is required';
+  } else if (text.length < 2 || text.length > 1000) {
+    textErr = 'Message must be between 2 and 1000 characters';
+  }
+
+  if (senderErr || textErr) {
+    res.render('form', {
+      title: 'Error creating message',
+      sender,
+      text,
+      senderErr,
+      textErr,
+    });
+    return;
+  }
+
+  next();
+}
+
+router.post('/new', validateFormData, async (req, res, next) => {
+  try {
+    const { sender, text } = req.body;
+    const message = new Message({ sender, text });
+    await message.save();
+    res.redirect('/');
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
